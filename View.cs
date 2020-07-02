5efe526d0671dafb0cad9099be5a338938a65111
Anaerob <thexbxeh@gmail.com>
2020-07-02 22:24:38 +1000

    init project

---
+++ View.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace warehouse
+{
+    class FormView : Form1, IView
+    {
+        int[] IView.InputPickets => throw new NotImplementedException();
+    }
+}
